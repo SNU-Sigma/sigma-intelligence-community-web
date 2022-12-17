51c55321f90fad031ffcc7509cf1683a01f69f89
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-18 03:03:32 +0900

    feat: 이미지 업로드 유틸 함수 생성 및 예시 컴포넌트 생성

---
+++ src/examples/ImageUploadForm.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { createImageUpload } from '../lib/util/createImageUpload'
+
+    let uploadedImageUrl: string | undefined = undefined
+
+    const { files$, validFileTypes, handleUpload } = createImageUpload()
+
+    const handleClick = async () => {
+        uploadedImageUrl = await handleUpload()
+    }
+</script>
+
+<main>
+    <div class="container">
+        <input
+            accept={validFileTypes.join(',')}
+            type="file"
+            bind:files={$files$}
+        />
+        <button on:click={handleClick}>Upload</button>
+        {#if uploadedImageUrl !== undefined}
+            <img src={uploadedImageUrl} alt="이미지" />
+        {/if}
+    </div>
+</main>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ src/lib/util/createImageUpload.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { get, writable } from 'svelte/store'
+
+export const createImageUpload = () => {
+    const files$ = writable<FileList | undefined>(undefined)
+
+    const validFileTypes = ['image/jpeg', 'image/png']
+
+    const handleUpload = async (): Promise<string | undefined> => {
+        for (const file of get(files$) ?? []) {
+            if (validFileTypes.includes(file.type)) {
+                try {
+                    const {
+                        data: { url, uploadedUrl },
+                    } = await axios.get(
+                        `/images/pre-signed-url?fileName=${file.name}`,
+                    )
+                    await axios.put(url, file, {
+                        validateStatus: (status) => status < 300,
+                    })
+                    return uploadedUrl
+                } catch (e) {
+                    console.error('Upload failed', e)
+                }
+            } else {
+                console.error('Not valid file')
+            }
+        }
+    }
+
+    return { files$, validFileTypes, handleUpload }
+}
