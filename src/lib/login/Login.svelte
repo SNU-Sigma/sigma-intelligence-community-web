69e62815cd1363c04bf624c483daf2b8441d0cbb
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-02 01:14:19 +0900

    feat: Use felte for form data handling

--- src/lib/login/Login.svelte
+++ src/lib/login/Login.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-    import axios from 'axios'
+    import { createForm } from 'felte'
+
+    type LoginInputData = {
+        email: string
+        password: string
+    }
+
+    const { form } = createForm<LoginInputData>({
+        onSubmit: (values) => {
+            const { email, password } = values
+            console.log({ email, password })
+        },
+    })
 </script>
 
-<form>
-    <input />
+<form use:form class="w-64 flex flex-col gap-4">
+    <label>
+        <span>아이디</span>
+        <input type="text" name="email" />
+    </label>
+    <label>
+        <span>비밀번호</span>
+        <input type="password" name="password" />
+    </label>
+    <button type="submit" class="btn btn-filled-primary">로그인</button>
 </form>
