6655aafdf0570f9f0fe239d8532fa630b7696ee1
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-02 00:50:13 +0900

    feat: Show login page conditionally

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Login from '../lib/login/Login.svelte'
+
+    $: shouldShowLoginPage = true
+</script>
+
+{#if shouldShowLoginPage}
+    <Login />
+{:else}
+    <slot />
+{/if}
