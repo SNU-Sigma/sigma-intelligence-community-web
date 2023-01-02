1d211d28c5e44df922787920c7709fe43b538503
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-03 00:36:04 +0900

    feat: Add LightSwitch to layout

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { LightSwitch } from '@skeletonlabs/skeleton'
     import { derived } from 'svelte/store'
     import Login from '../lib/login/Login.svelte'
     import { LoginInfo$ } from '../lib/login/store/LoginInfo$'
@@ -9,7 +10,12 @@
     )
 </script>
 
-<div class="flex items-center justify-center h-screen w-96 m-auto">
+<div
+    class="relative flex items-center justify-center h-screen max-w-md mx-auto bg-gray-50 dark:bg-gray-900"
+>
+    <div class="absolute bottom-4 left-4">
+        <LightSwitch />
+    </div>
     {#if $shouldShowLoginPage}
         <Login />
     {:else}
