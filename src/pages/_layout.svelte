06b28d612de68d8fec52ad23a506108f892315c6
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-04 09:54:38 +0900

    improvement: 로그인 실패 시 토스트 표시

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { LightSwitch } from '@skeletonlabs/skeleton'
+    import { LightSwitch, Toast } from '@skeletonlabs/skeleton'
     import { derived } from 'svelte/store'
     import Login from '../lib/login/Login.svelte'
     import { LoginInfo$ } from '../lib/login/store/LoginInfo$'
@@ -13,6 +13,7 @@
 <div
     class="relative flex items-center justify-center h-screen max-w-md mx-auto bg-gray-50 dark:bg-gray-900"
 >
+    <Toast />
     <div class="absolute bottom-4 left-4">
         <LightSwitch />
     </div>
