5a5f68624cf950b5f737a0f37ecb249d2b6010cd
Jihoon Seo <jihoon416@snu.ac.kr>
2023-01-06 01:35:34 +0900

    feat: 401 에러 떨어지면 로그아웃하고 토스트 보여주기

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,22 @@ import './theme.postcss'
 import './app.postcss'
 import axios from 'axios'
 import App from './App.svelte'
+import { LoginInfo$ } from './lib/domain/login/LoginInfo$'
+import { toastStore } from '@skeletonlabs/skeleton'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 axios.defaults.withCredentials = true
 axios.defaults.validateStatus = (status) => status < 400
+axios.interceptors.response.use(undefined, (error) => {
+    if (error.response.status === 401) {
+        LoginInfo$.setIsLoggedIn(false)
+        toastStore.trigger({
+            message: '토큰이 만료되었습니다. 다시 로그인해주세요.',
+            preset: 'error',
+        })
+    }
+    return Promise.reject(error)
+})
 
 const app = new App({
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
