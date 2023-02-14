d09f6868950d3b28f27f6af3c772bc1917da098d
Seungyeon <jsy1105@snu.ac.kr>
2023-02-14 17:21:49 +0900

    text size, picture size don't change

--- src/pages/profile.svelte
+++ src/pages/profile.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { goto, params } from '@roxi/routify'
     import { createForm } from 'felte'
     import { LoginInfo$ } from '../lib/domain/login/LoginInfo$'
     import { AuthAPIImpl } from '../lib/infrastructure/sigma-api/AuthAPIImpl'
@@ -11,15 +10,21 @@
     const member: Promise<ProfileDto> = ProfileAPIImpl.getMyProfile()
 </script>
 
-<div class="md absolute left-5 top-5">SIGMA INTELLIGENCE</div>
+<div class="bg-lime-300">
+    <p class="md absolute left-5 top-5 font-bold">SIGMA INTELLIGENCE</p>
 
-<div class="flex-col items-center">
-    {#await member}
-        <p>loading</p>
-    {:then item}
-        <p class="test-sm font-bold">{item.name}</p>
-        <p class="test-md">{item.major} {item.freshmanYear}학번</p>
-    {:catch error}
-        <p style="color: red">{error.message}</p>
-    {/await}
+    <div class="flex flex-col items-center">
+        {#await member}
+            <p>loading</p>
+        {:then item}
+            <img
+                class="h-30 w-30 p-5... object-cover"
+                src={item.profileImageUrl}
+            />
+            <p class="... text-sm font-bold">{item.name}</p>
+            <p class="... text-lg">{item.major} {item.freshmanYear}학번</p>
+        {:catch error}
+            <p style="color: red">{error.message}</p>
+        {/await}
+    </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,7 @@ const config = {
     },
 
     plugins: [
+        require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
         ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')(),
     ],
