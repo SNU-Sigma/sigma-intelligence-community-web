307c2203184c2957f9f5a76dd45f44ad228861c2
Seungyeon <jsy1105@snu.ac.kr>
2023-02-14 18:03:59 +0900

    font and bg are complete,but alignment isn't

--- src/pages/profile.svelte
+++ src/pages/profile.svelte
@@ -10,19 +10,15 @@
     const member: Promise<ProfileDto> = ProfileAPIImpl.getMyProfile()
 </script>
 
-<div class="bg-lime-300">
+<div class="bg h-full w-full bg-gradient-to-br from-pink-300 to-indigo-400">
     <p class="md absolute left-5 top-5 font-bold">SIGMA INTELLIGENCE</p>
-
     <div class="flex flex-col items-center">
         {#await member}
             <p>loading</p>
         {:then item}
-            <img
-                class="h-30 w-30 p-5... object-cover"
-                src={item.profileImageUrl}
-            />
-            <p class="... text-sm font-bold">{item.name}</p>
-            <p class="... text-lg">{item.major} {item.freshmanYear}학번</p>
+            <img class="mb-5 w-1/3 rounded-full" src={item.profileImageUrl} />
+            <div class="mb-2 text-3xl font-bold">{item.name}</div>
+            <div class="text-xl">{item.major} {item.freshmanYear}학번</div>
         {:catch error}
             <p style="color: red">{error.message}</p>
         {/await}
