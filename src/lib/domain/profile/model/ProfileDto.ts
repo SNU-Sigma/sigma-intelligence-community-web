f2713e3c401b326bee9fa9e13fecbf72d8919033
Seungyeon <102790547+seunnyeon@users.noreply.github.com>
2023-02-16 00:02:49 +0900

    Feature/profile page (#29)
    * feat: ProfileAPIImpl 및 Dto 미리 생성
    
    * first draft of profile page
    
    * able to upload information of a user
    
    * text size, picture size don't change
    
    * font and bg are complete,but alignment isn't
    
    * erase import part
    
    * fixed
    
    * feedback
    
    * fix: Remove my-skeleton-app
    
    * fix: Revert package.json change
    
    Revert "first draft of profile page"
    
    This reverts commit 33223cdaeb81dbd26a840db3df146ee79b01eb24.
    
    * improvement: 프로필 사진 항상 원 형태로 나오도록 사소한 개선
    
    ---------
    
    Co-authored-by: Jihoon Seo <jihoon416@snu.ac.kr>

---
+++ src/lib/domain/profile/model/ProfileDto.ts
@@ -0,0 +1,18 @@
+export type ProfileDto = {
+    /**
+     * 이름
+     */
+    name: string
+    /**
+     * 학번
+     */
+    freshmanYear: number
+    /**
+     * 과/전공
+     */
+    major: string
+    /**
+     * 프로필 사진 URL
+     */
+    profileImageUrl: string
+}
---
+++ src/pages/profile.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { ProfileDto } from '../lib/domain/profile/model/ProfileDto'
+    import { ProfileAPIImpl } from '../lib/infrastructure/sigma-api/ProfileAPIImpl'
+    import OverlaySpinner from '../lib/ui/common/OverlaySpinner.svelte'
+
+    const member: Promise<ProfileDto> = ProfileAPIImpl.getMyProfile()
+</script>
+
+<div class="bg h-full w-full bg-gradient-to-br from-pink-300 to-indigo-400">
+    <p class="md absolute left-5 top-5 font-bold">SIGMA INTELLIGENCE</p>
+    <div class="flex h-full flex-col items-center justify-center">
+        {#await member}
+            <OverlaySpinner />
+        {:then item}
+            <img
+                class="mb-5 aspect-square w-1/3 rounded-full object-cover"
+                src={item.profileImageUrl}
+                alt=""
+            />
+            <div class="mb-2 text-3xl font-bold">{item.name}</div>
+            <div class="text-xl">{item.major} {item.freshmanYear}학번</div>
+        {:catch error}
+            <p style="color: red">{error.message}</p>
+        {/await}
+    </div>
+</div>
