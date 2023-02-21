6aea0699c6e805e5cb5baabcd5a7306b6a941b88
Seungyeon <jsy1105@snu.ac.kr>
2023-02-21 17:30:28 +0900

    update 함수 찾음. 그러나 작동은 잘 안 됨..

--- src/pages/edit_profile.svelte
+++ src/pages/edit_profile.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
-    import type { UpdateProfileDto } from '../lib/domain/profile/model/ProfileDto'
-    import OverlaySpinner from '../lib/ui/common/OverlaySpinner.svelte'
+    import type { UpdateProfileDto } from '../lib/domain/profile/model/UpdateProfileDto'
     import { goto, params } from '@roxi/routify'
-    import ImageUploadForm from './examples/ImageUploadForm.svelte'
+    import { ProfileAPIImpl } from '../lib/infrastructure/sigma-api/ProfileAPIImpl'
+    import { createImageUpload } from '../lib/util/createImageUpload'
 
     const member: UpdateProfileDto = {
         profileImageUrl: $params.url,
         major: $params.major,
         freshmanYear: $params.Year,
     }
+    const { files$, validFileTypes, handleUpload } = createImageUpload()
+    const handleClick = async () => {
+        member.profileImageUrl = await handleUpload()
+    }
 </script>
 
-<p>profile_url: <ImageUploadForm /></p>
+<div>
+    profile_url:
+    <div class="my-auto flex flex-col items-center">
+        <input
+            accept={validFileTypes.join(',')}
+            type="file"
+            bind:files={$files$}
+        />
+        <button class="btn variant-ghost-primary mt-4" on:click={handleClick}>
+            Upload
+        </button>
+    </div>
+</div>
 <p>
     freshmanYear: <input type="text" bind:value={member.freshmanYear} />
 </p>
@@ -21,6 +37,7 @@
 <button
     on:click={() => {
         $goto('/profile')
+        ProfileAPIImpl.updateMyProfile(member)
     }}
 >
     완료
