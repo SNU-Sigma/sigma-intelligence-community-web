e830a010fa81cf00d38abffa1826aac500108e53
Jihoon Seo <jihoon416@snu.ac.kr>
2023-02-26 15:58:54 +0900

    improvement: LightSwitch가 프로필 페이지에만 있도록

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { isActive } from '@roxi/routify'
-    import { LightSwitch, Toast } from '@skeletonlabs/skeleton'
+    import { Toast } from '@skeletonlabs/skeleton'
     import { derived } from 'svelte/store'
     import { LoginInfo$ } from '../lib/domain/login/LoginInfo$'
     import Login from '../lib/ui/login/Login.svelte'
@@ -49,9 +49,6 @@
     class="relative mx-auto flex min-h-screen max-w-md flex-1 flex-col bg-gray-50 dark:bg-gray-900"
 >
     <Toast />
-    <div class="fixed z-50 m-2 self-end">
-        <LightSwitch />
-    </div>
     {#if $shouldShowLoginPage}
         <Login />
     {:else}
